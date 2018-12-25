549497ffbe820b676d54110bd6c346234f85d986
stonecity <bluecart@live.com>
2018-12-26 00:05:58 +0800

    代码格式化

--- SearchFile/SearchFile.cpp
+++ SearchFile/SearchFile.cpp
@@ -22,45 +22,44 @@ void ShowErrorMessage(DWORD nErrorID)
 BOOL IsBackslash(LPCTSTR lpstr)
 {
 	int idx = wcslen(lpstr) - 1;
-	if(lpstr[idx] == '\\')
+	if (lpstr[idx] == '\\')
 		return TRUE;
 	return FALSE;
 }
 
-
 void ListAllFiles(LPCTSTR lpPath)
-{ 
+{
 	TCHAR szFind[MAX_PATH];
 	lstrcpy(szFind, lpPath);
 	lstrcat(szFind, _T("\\*.*"));
 
 	WIN32_FIND_DATA  data;
 	HANDLE hFindFile = FindFirstFile(szFind, &data);
-	
+
 	if (INVALID_HANDLE_VALUE == hFindFile)
-	{	
+	{
 		DWORD dError = GetLastError();
 		ShowErrorMessage(dError);
 		return;
 	}
 
-	do{
+	do {
 		if (data.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY
 			&& lstrcmp(data.cFileName, _T("."))
 			&& lstrcmp(data.cFileName, _T("..")))
 		{
 			lstrcpy(szFind, lpPath);
 			lstrcat(szFind, _T("\\"));
-			lstrcat(szFind, data.cFileName); 
+			lstrcat(szFind, data.cFileName);
 			ListAllFiles(szFind);
 		}
 
-		if(data.cFileName[0] != '.')
+		if (data.cFileName[0] != '.')
 		{
 			wcout.imbue(locale("chs"));
 			wcout << lpPath << _T("\\") << data.cFileName << endl;
 		}
-	}while(FindNextFile(hFindFile, &data));
+	} while (FindNextFile(hFindFile, &data));
 
 	FindClose(hFindFile);
 }
@@ -72,15 +71,15 @@ void SearchFile(CString pstr)
 
 	strWildcard += _T("\\*.*");
 	BOOL bWorking = finder.FindFile(strWildcard);
-	while(bWorking)
+	while (bWorking)
 	{
 		bWorking = finder.FindNextFileW();
-		if(finder.IsDots())
+		if (finder.IsDots())
 			continue;
-		
-		if(finder.IsArchived())
-			cout << "----"; 
-		
+
+		if (finder.IsArchived())
+			cout << "----";
+
 		CString sFileName = finder.GetFileName();
 		wcout.imbue(locale("chs"));
 
