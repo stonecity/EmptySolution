65884d78ddbf10279c5d72902d54faa5f75d190c
stonecity <bluecart@live.com>
2018-12-26 00:07:44 +0800

    格式化代码

--- SearchFile/TestFindFile.cpp
+++ SearchFile/TestFindFile.cpp
@@ -7,56 +7,56 @@
 #include <iostream>
 using namespace std;
 
-BOOL IsRoot(LPCTSTR lpszPath) 
-{ 
-	TCHAR szRoot[4]; 
-	wsprintf(szRoot, _T("%c:\\"), lpszPath[0]); 
-	return (lstrcmp(szRoot, lpszPath) == 0); 
-} 
-
-void FindInAll(::LPCTSTR lpszPath) 
+BOOL IsRoot(LPCTSTR lpszPath)
 {
-	TCHAR szFind[MAX_PATH]; 
-	lstrcpy(szFind, lpszPath); 
-	if (!IsRoot(szFind)) 
-		lstrcat(szFind, _T("\\")); 
+	TCHAR szRoot[4];
+	wsprintf(szRoot, _T("%c:\\"), lpszPath[0]);
+	return (lstrcmp(szRoot, lpszPath) == 0);
+}
+
+void FindInAll(::LPCTSTR lpszPath)
+{
+	TCHAR szFind[MAX_PATH];
+	lstrcpy(szFind, lpszPath);
+	if (!IsRoot(szFind))
+		lstrcat(szFind, _T("\\"));
 	lstrcat(szFind, _T("*.*")); // 找所有文件 
 
-	WIN32_FIND_DATA wfd; 
-	HANDLE hFind = FindFirstFile(szFind, &wfd); 
+	WIN32_FIND_DATA wfd;
+	HANDLE hFind = FindFirstFile(szFind, &wfd);
 
 	if (hFind == INVALID_HANDLE_VALUE) // 如果没有找到或查找失败 
-		return; 
+		return;
 
-	do 
-	{ 
-		if (wfd.cFileName[0] == '.') 
+	do
+	{
+		if (wfd.cFileName[0] == '.')
 			continue; // 过滤这两个目录 
-		if (wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) 
-		{ 
-			TCHAR szFile[MAX_PATH]; 
-			if(IsRoot(lpszPath)) 
-				wsprintf(szFile, _T("%s%s"), lpszPath, wfd.cFileName); 
-			else 
-				wsprintf(szFile, _T("%s\\%s"), lpszPath, wfd.cFileName); 
-			
+		if (wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+		{
+			TCHAR szFile[MAX_PATH];
+			if (IsRoot(lpszPath))
+				wsprintf(szFile, _T("%s%s"), lpszPath, wfd.cFileName);
+			else
+				wsprintf(szFile, _T("%s\\%s"), lpszPath, wfd.cFileName);
+
 			FindInAll(szFile); // 如果找到的是目录，则进入此目录进行递归 
-		} 
-		else 
-		{ 
-			TCHAR szFile[MAX_PATH]; 
-			if (IsRoot(lpszPath)) 
-				wsprintf(szFile, _T("%s%s"), lpszPath, wfd.cFileName); 
-			else 
-				wsprintf(szFile, _T("%s\\%s"), lpszPath, wfd.cFileName); 
-			 
+		}
+		else
+		{
+			TCHAR szFile[MAX_PATH];
+			if (IsRoot(lpszPath))
+				wsprintf(szFile, _T("%s%s"), lpszPath, wfd.cFileName);
+			else
+				wsprintf(szFile, _T("%s\\%s"), lpszPath, wfd.cFileName);
+
 			wcout.imbue(locale("chs"));
 			wcout << szFile << endl;	//输出结果
-		} 
-	}while (FindNextFile(hFind, &wfd)); 
+		}
+	} while (FindNextFile(hFind, &wfd));
 
 	FindClose(hFind); // 关闭查找句柄 
-} 
+}
 
 int l_tmain(int argc, char* argv[]) 
 { 
